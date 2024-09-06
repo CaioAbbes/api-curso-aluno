128d3f74cfa912dd742ff1d88ffe4ff2aa1f9568
CaioAbbes <caio.abbes.pereira@outlook.com>
2024-09-06 20:07:14 -0300

    alterar porta

--- app.py
+++ app.py
@@ -124,4 +124,4 @@ def get_aluno_matricula_curso():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5004)
